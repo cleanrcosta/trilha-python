79272458257b1647e71f9020382d46800f231b67
Clean Rodrigo Costa <cleanrcosta@gmail.com>
2025-03-15 11:45:03 -0300

    Update operadores_associacao.py

--- 00 - Fundamentos/operadores_associacao.py
+++ 00 - Fundamentos/operadores_associacao.py
@@ -1,6 +1,13 @@
 frutas = ["limao", "uva"]
-curso = "Curso de python"
+curso = "Curso de Python"
+saques = [1500, 100]
+
+
+"Python" in curso
+
+"maçã" not in frutas
 
 print("laranja" not in frutas)
 print("limao" in frutas)
-print("Python" in curso)
+print("Python" in curso) #Diferencia letra maiuscula de minuscula
+
